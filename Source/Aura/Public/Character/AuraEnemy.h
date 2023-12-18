041d126f3ebc672d1ee62e781edc53d6a052b87e
Jimmy Reilly <reilly@whitewavemedia.com>
2023-12-18 18:45:38 -0500

    3-23 Init Ability Actor Info

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -17,6 +17,12 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 
 public:
 	AAuraEnemy();
+	
+	// Enemy interface
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
+	// end Enemy interface
+
+protected:
+	virtual void BeginPlay() override;
 };
